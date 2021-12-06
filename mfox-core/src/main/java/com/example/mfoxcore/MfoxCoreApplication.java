f2ee5a4eb9dd60e7653257eb97d8f921bca171c2
16868 <1686888041@qq.com>
2021-12-06 17:47:35 +0800

    创建项目

---
+++ mfox-core/src/main/java/com/example/mfoxcore/MfoxCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.mfoxcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfoxCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfoxCoreApplication.class, args);
+    }
+
+}
